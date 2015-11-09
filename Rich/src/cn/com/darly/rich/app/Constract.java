128c9c8076267ad9921dcada0ea8daffb029e7b4
gongsi <darlyyuhui@hotmail.com>
2015-11-09 17:33:26 +0800

    所有工具类全部放到Rich项目中

---
+++ Rich/src/cn/com/darly/rich/app/Constract.java
@@ -0,0 +1,19 @@
+package cn.com.darly.rich.app;
+
+
+/**
+ * @ClassName: Literal
+ * @Description: TODO(Handler的常量类)
+ * @author 张宇辉 zhangyuhui@octmami.com
+ * @date 2014年11月26日 上午9:03:57
+ *
+ */
+public class Constract {
+
+	public static int width = -1;
+
+	public static int height = -1;
+
+	public static int desty = -1;
+
+}
