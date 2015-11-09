543a1cbe464dcaa581cedfd8facf9d6843fa7587
gongsi <darlyyuhui@hotmail.com>
2015-11-09 16:49:28 +0800

    token添加引导页面和版本升级后台下载。

--- Token/src/com/darly/token/ui/MainActivity.java
+++ Token/src/com/darly/token/ui/MainActivity.java
@@ -1,10 +1,15 @@
 package com.darly.token.ui;
 
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 import com.darly.token.R;
+import com.darly.token.service.UpdateService;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +17,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Log.i("1111111", Calendar.DAY_OF_MONTH + "");
+
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DAY_OF_MONTH, Calendar.DAY_OF_MONTH+20);
+		Log.i("1111111", calendar.get(Calendar.DAY_OF_MONTH) + "");
+
+		Intent i = new Intent(this, UpdateService.class);
+		i.putExtra("url",
+				"http://gdown.baidu.com/data/wisegame/4091da454312f1c1/QQ_288.apk");
+//		 startService(i);
 	}
 
 	@Override
